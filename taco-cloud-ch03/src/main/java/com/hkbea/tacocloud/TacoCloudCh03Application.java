0b68ba7e7f1c6b23b676315b3b1e1e4a5302bdbd
jxxchallenger <jxxchallenger@foxmail.com>
2018-11-17 12:36:10 +0800

    init ch03 20181117

---
+++ taco-cloud-ch03/src/main/java/com/hkbea/tacocloud/TacoCloudCh03Application.java
@@ -0,0 +1,12 @@
+package com.hkbea.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudCh03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudCh03Application.class, args);
+	}
+}
