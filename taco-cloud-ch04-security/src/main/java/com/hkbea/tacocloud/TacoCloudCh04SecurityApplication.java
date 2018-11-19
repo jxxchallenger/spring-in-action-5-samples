d9bf9b154d4f371981d7982312c22e2ef8ada730
jxxchallenger <jxxchallenger@foxmail.com>
2018-11-19 22:32:36 +0800

    start ch04

---
+++ taco-cloud-ch04-security/src/main/java/com/hkbea/tacocloud/TacoCloudCh04SecurityApplication.java
@@ -0,0 +1,12 @@
+package com.hkbea.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudCh04SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudCh04SecurityApplication.class, args);
+	}
+}
