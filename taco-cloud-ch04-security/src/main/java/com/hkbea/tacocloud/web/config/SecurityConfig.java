826d6b77eb3c4dc26cabcb5f128621e067b3b0eb
jxxchallenger <jxxchallenger@foxmail.com>
2018-11-25 21:23:32 +0800

    ch04 customize login page

--- taco-cloud-ch04-security/src/main/java/com/hkbea/tacocloud/web/config/SecurityConfig.java
+++ taco-cloud-ch04-security/src/main/java/com/hkbea/tacocloud/web/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hkbea.tacocloud.web.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -19,24 +21,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	/*@Bean
+	public JdbcUserDetailsManager getUserDetailsManager() {
+		return new JdbcUserDetailsManager(dataSource);
+	}*/
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.jdbcAuthentication()
-			.passwordEncoder(NoOpPasswordEncoder.getInstance())
+			.passwordEncoder(getPasswordEncoder())
 			.dataSource(dataSource);
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		// TODO Auto-generated method stub
 		super.configure(web);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		//http.authorizeRequests().antMatchers("/design", "/orders").hasRole("USER").antMatchers("/", "/**").permitAll().and().formLogin();
+		http.authorizeRequests()
+			.antMatchers("/design", "/orders")
+			.access("hasRole('USER')")
+			.antMatchers("/", "/**")
+			.permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/loginForm")
+			.loginProcessingUrl("/login");//如果用loginPage()方法指定了登陆页面, 而未调用loginProcessingUrl()方法指定处理登陆请求url, 则处理登陆请求url与登陆页面相同
 	}
 
 }
